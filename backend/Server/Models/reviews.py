851f19b39313252a089087df7b031e878ff7b1c2
Kennedy <kiplangatkennedy019@gmail.com>
2023-07-27 23:25:29 +0300

    reviews

---
+++ backend/Server/Models/reviews.py
@@ -0,0 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
+from sqlalchemy.orm import validates
+import re
+from app import db 
+
+
+class reviews(db.Model):
+    __tablename__="reviews"
+
+    
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    ngotb_Id = db.Column(db.Integer, db.ForeignKey('ngotb.id'))
+    review = db.Column(db.String(255), nullable=False)
+
+    def __repr__(self):
+        
+        return f"Review(id={self.id}, user_id={self.user_id}, ngotb_id={self.ngotb_id}, review='{self.review}')"
