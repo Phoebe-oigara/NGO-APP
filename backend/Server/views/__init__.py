92e9ba6d7e293064eaf819a925c7a9c1824e84f6
Erickkirui <erickkirui653@gmail.com>
2023-07-28 15:27:17 +0300

    completed endpoints

--- backend/Server/views/__init__.py
+++ backend/Server/views/__init__.py
@@ -1,11 +1,12 @@
 from flask import Blueprint
 from flask_restful import Api
 # import all views file here
+from Server.views.ngoviews import ViewAllNgo,ViewNgoById,RegisterNgo
 
 api_endpoints = Blueprint('auth', __name__, url_prefix='/ngoconnect')
 api = Api(api_endpoints)
 
 # specify all view points here
-
-
-
+api.add_resource(ViewAllNgo, '/ngolist')
+api.add_resource(RegisterNgo, '/register')
+api.add_resource(ViewNgoById, '/ngo/<int:ngo_id>')
