33f99018eed9411d071b744718f9ea8e87279233
Erickkirui <erickkirui653@gmail.com>
2023-07-28 15:51:17 +0300

    ngo endpoints and resolved conflicts

--- backend/Server/views/__init__.py
+++ backend/Server/views/__init__.py
@@ -2,6 +2,8 @@ from flask import Blueprint
 from flask_restful import Api
 # import all views file here
 from Server.views.ngoviews import ViewAllNgo,ViewNgoById,RegisterNgo
+from Server.views.users import GetAllUsers,AddUser,UserResourcesById
+
 
 api_endpoints = Blueprint('auth', __name__, url_prefix='/ngoconnect')
 api = Api(api_endpoints)
@@ -10,3 +12,7 @@ api = Api(api_endpoints)
 api.add_resource(ViewAllNgo, '/ngolist')
 api.add_resource(RegisterNgo, '/register')
 api.add_resource(ViewNgoById, '/ngo/<int:ngo_id>')
+
+api.add_resource(GetAllUsers, '/users')
+api.add_resource(AddUser, '/users')
+api.add_resource(UserResourcesById, '/users/<int:user_id>')
