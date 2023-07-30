b566035b09ee68f534c62f88a54bcfe4bc3abe21
Phoebe Oigara <phoebeoigara@gmail.com>
2023-07-31 02:05:17 +0300

    created the signup page and established routes to the landing and sign up pages

---
+++ frontend/client/src/components/UserSignup.js
@@ -0,0 +1,76 @@
+
+import React, { useState } from 'react';
+
+const UserSignup = ({ onSubmit }) => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Pass the form data to the parent component's onSubmit function
+    onSubmit(formData);
+  };
+
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Name:</label>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Confirm Password:</label>
+          <input
+            type="password"
+            name="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <button type="submit">Create Account</button>
+          <button type="button">Register NGO</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default UserSignup;
