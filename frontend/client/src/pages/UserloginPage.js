8ad09a25313c11b5f1723409b56977868325e5cf
Alex Kibe <alex.kibe01@student.moringaschool.com>
2023-08-01 17:34:03 +0300

    trial3

--- frontend/client/src/pages/UserloginPage.js
+++ frontend/client/src/pages/UserloginPage.js
@@ -9,12 +9,22 @@ function UserloginPage({ onSubmit }) {
   };
  
 
-   return (
-     <div>
-       {/* <h2>User Login Page</h2> */}
-       <LoginForm onSubmit={handleSubmit} /> 
-     </div>
-   );
+  return (
+    <div className="container"> {/* Add the container class */}
+      <div className="row justify-content-center">
+        <div className="col-md-6"> {/* Add Bootstrap column class */}
+          <div className="form-container"> {/* Add the form container class */}
+          
+            <LoginForm onSubmit={handleSubmit} />
+          </div>
+        </div>
+        <div className="col-md-6"> {/* Add Bootstrap column class for the image */}
+          {/* Add your image here */}
+        </div>
+      </div>
+    </div>
+  );
 }
+
 
 export default UserloginPage;
