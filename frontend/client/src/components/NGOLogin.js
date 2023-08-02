f09d4a115484d463de834dea9edd634473871039
Melaine Kanana <melaine.kanana@student.moringaschool.com>
2023-08-02 06:53:54 +0300

    ngologintrial

--- frontend/client/src/components/NGOLogin.js
+++ frontend/client/src/components/NGOLogin.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const NGOLogin = () => {
+const NGOLogin = ({ onSubmit }) => {
   const [formData, setFormData] = useState({
     name: '',
     password: '',
@@ -13,32 +13,42 @@ const NGOLogin = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Handle login here (e.g., send login data to the server)
-    console.log(formData);
+    // Pass the form data to the parent component's onSubmit function
+    onSubmit(formData);
   };
 
   return (
     <div>
       <h2>NGO Login</h2>
-      <div>
-        <label>
-          Name:
-          <input type="text" name="name" value={formData.name} onChange={handleChange} />
-        </label>
-      </div>
-      <div>
-        <label>
-          Password:
-          <input type="password" name="password" value={formData.password} onChange={handleChange} />
-        </label>
-      </div>
-      <div>
-        <button type="submit" onClick={handleSubmit}>
-          Login
-        </button>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Name:</label>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <button type="submit">Login</button>
+        </div>
+      </form>
     </div>
   );
 };
 
 export default NGOLogin;
+
+
