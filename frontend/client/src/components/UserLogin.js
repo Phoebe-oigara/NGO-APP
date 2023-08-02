eefd4104526b0c5e5ecb2bc777218b0e3ad92c00
Alex Kibe <alex.kibe01@student.moringaschool.com>
2023-08-02 09:42:02 +0300

    polished on Ngologin form

--- frontend/client/src/components/UserLogin.js
+++ frontend/client/src/components/UserLogin.js
@@ -1,63 +1,4 @@
-// import React, { useState } from 'react';
 
-// const LoginForm = () => {
-//   const [formData, setFormData] = useState({
-//     name: '',
-//     password: '',
-//   });
-
-//   const handleChange = (e) => {
-//     const { name, value } = e.target;
-//     setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
-//   };
-
-//   const handleLogin = (e) => {
-//     e.preventDefault();
-//     // Handle login logic here (e.g., send login request to the server)
-//     console.log('Login data:', formData);
-//   };
-
-//   const handleSignup = () => {
-//     // Handle signup navigation or logic here
-//     console.log('Redirect to signup page or show signup modal');
-//   };
-  
-
-//   return (
-//     <div>
-//       <h2>User Login</h2>
-//       <form onSubmit={handleLogin}>
-//         <div>
-//           <label>
-//             Name:
-//             <input type="text" name="name" value={formData.name} onChange={handleChange} />
-//           </label>
-//         </div>
-//         <div>
-//           <label>
-//             Password:
-//             <input
-//               type="password"
-//               name="password"
-//               value={formData.password}
-//               onChange={handleChange}
-//             />
-//           </label>
-//         </div>
-//         <div>
-//           <button type="submit">Login</button>
-//         </div>
-//       </form>
-//       <div>
-//         <button onClick={handleSignup}>Sign Up</button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default LoginForm;
-// LoginForm.js
-// LoginForm.js
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -78,41 +19,7 @@ const LoginForm = ({ onSubmit }) => {
     // Add your login logic here (e.g., sending login request to the server)
   };
 
-//   return (
-//       <div>
-//         <form onSubmit={handleSubmit}>
-//           <h2 className="text-center mb-4">User Login</h2> {/* User Login header */}
-//           <div className="mb-3">
-//             <input
-//               type="email"
-//               className="form-control"
-//               name="email"
-//               value={formData.email}
-//               onChange={handleChange}
-//               placeholder="Name"
-//               required
-//             />
-//           </div>
-//           <div className="mb-3">
-//             <input
-//               type="password"
-//               className="form-control"
-//               name="password"
-//               value={formData.password}
-//               onChange={handleChange}
-//               placeholder="Password"
-//               required
-//             />
-//           </div>
-//           <button type="submit" className="btn btn-primary btn-block">
-//             Login
-//           </button>
-//         </form>
-//       </div>
-//   );
-// };
 
-// export default LoginForm;
 
 return (
   <div className= 'container'>
--- frontend/client/src/pages/NGOLoginPage.js
+++ frontend/client/src/pages/NGOLoginPage.js
@@ -7,12 +7,29 @@ function NGOloginPage({ onSubmit }) {
     onSubmit(formData);
   };
 
-  return (
-    <div> 
-      <h2>NGO Login</h2>
-      <NGOlogin onSubmit={handleSubmit} /> 
+//   return (
+//     <div> 
+//       <h2>NGO Login</h2>
+//       <NGOlogin onSubmit={handleSubmit} /> 
+//     </div>
+//   );
+// }
+return (
+  <div className="container"> {/* Add the container class */}
+    <div className="row justify-content-center">
+      <div className="col-md-6"> {/* Add Bootstrap column class */}
+        <div className="form-container"> {/* Add the form container class */}
+        {/* <h2>NGO Login</h2> */}
+        
+          <NGOlogin onSubmit={handleSubmit} />
+        </div>
+      </div>
+      <div className="col-md-6"> {/* Add Bootstrap column class for the image */}
+        {/* Add your image here */}
+      </div>
     </div>
-  );
+  </div>
+);
 }
 
 export default NGOloginPage;
